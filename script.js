c8efcece418c3a629b9cbb1072415ad5b9e59380
Nicholas Moen <arcanemachine@tutanota.com>
2022-07-31 00:28:15 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+/* eslint no-undef: 0 */
+/* eslint no-unused-vars: 0 */
+
+"use strict";
+
+function aMathWhizComponent() { // options = {}) {
+  return {
+    // ...options,
+    helloString: "Hello world!",
+  };
+}
