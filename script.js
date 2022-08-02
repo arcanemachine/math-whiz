40342e3af1c33a7f8a51f2c211463bf2d88869ad
Nicholas Moen <arcanemachine@tutanota.com>
2022-08-01 19:29:53 -0600

    Basic options menu feature working

--- script.js
+++ script.js
@@ -5,11 +5,24 @@
 
 function aMathWhizComponent() {
   return {
+    init() {
+      window.ccc = this;
+    },
+
     gameLoopIsActive: false,
-    
 
     gameStart() {
       this.gameLoopIsActive = true;
     },
+
+    // modals
+    get modalMenuMain() {
+      return bootstrap.Modal.getOrCreateInstance('#menu-modal');
+    },
+
+    get modalMenuGameExitConfirm() {
+      return bootstrap.Modal.getOrCreateInstance('#menu-game-exit-confirm');
+    }
+
   };
 }
