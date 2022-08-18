f894eacfc004315b64b3b1f259f8296f57a14e62
Nicholas Moen <arcanemachine@tutanota.com>
2022-08-18 01:59:41 -0600

    Add logic for data input

--- script.js
+++ script.js
@@ -9,26 +9,45 @@ function aMathWhizComponent() {
       window.ccc = this;
     },
 
+    // events
+    eventKeydownHandle(e) {
+      // only handle events in game loop
+      if (!this.gameLoopIsActive) return;
+
+      // ignore if modifier keys pressed
+      if (e.ctrlKey || e.shiftKey || e.altKey || e.metaKey) return;
+
+      e.preventDefault();
+
+      if (e.key === "Enter") this.answerSubmit();
+      else if (e.key === "Backspace") this.answerFieldBackspace();
+      else if (e.key === "Escape") this.answerFieldClear();
+      else if (Number(e.key)) this.answerFieldAppend(e.key);
+      else if (e.key === ".") this.answerFieldAppend(e.key);
+    },
+
     // game
     gameLoopIsActive: true,
-    gameStyle: 'addition',
+    gameStyle: "addition",
 
     gameStart() {
       this.gameLoopIsActive = true;
     },
 
-    gameExit() {
+    gameStop() {
       this.gameLoopIsActive = false; // exit the game
       this.modalMenuGameExitConfirm.hide(); // hide the modal
     },
 
     // modals
     get modalMenuMain() {
-      return bootstrap.Modal.getOrCreateInstance('#menu-modal');
+      return bootstrap.Modal.getOrCreateInstance("#menu-modal");
     },
 
     get modalMenuGameExitConfirm() {
-      return bootstrap.Modal.getOrCreateInstance('#menu-game-exit-confirm-modal');
+      return bootstrap.Modal.getOrCreateInstance(
+        "#menu-game-exit-confirm-modal"
+      );
     },
 
     // questions
@@ -36,7 +55,7 @@ function aMathWhizComponent() {
     questionCurrent: 0,
     questionNumberFirst: 0,
     questionNumberSecond: 0,
-    questionOperator: '+',
+    questionOperator: "+",
 
     // numbers
     numberFirstMin: 1,
@@ -44,9 +63,22 @@ function aMathWhizComponent() {
     numberSecondMin: 1,
     numberSecondMax: 1,
     numberDecimalCount: 0,
-    numberInputValue: "",
 
     // answers
     answerCorrectCount: 0,
+    answerFieldValue: "",
+    answerFieldClear() {
+      this.answerFieldValue = "";
+    },
+    answerFieldAppend(val) {
+      this.answerFieldValue += val;
+    },
+    answerFieldBackspace() {
+      this.answerFieldValue = this.answerFieldValue.substring(
+        0,
+        this.answerFieldValue.length - 1
+      );
+    },
+    answerSubmit() {},
   };
 }
